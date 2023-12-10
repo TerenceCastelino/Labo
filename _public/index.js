2c01c2844ae5fa1ea0b306ba633d123d9f70dfb1
terence planchestainer <t.planchestainer@stag.technofuturtic.education>
2023-12-10 18:39:49 +0100

    socket authentification

--- _public/index.js
+++ _public/index.js
@@ -1,8 +1,54 @@
-//FRONTEND
+// FRONTEND
 const socket = io();
 
+function handleLogin() {
+    const email = prompt('Entrez votre adresse e-mail :');
+    const password = prompt('Entrez votre mot de passe :');
 
-const userID = 3; // L'ID que vous souhaitez transmettre (pour le moment en dur)
+    if (email && password) {
+        // Effectuer la requête fetch vers localhost:3000/api/authentification/login
+        fetch('http://localhost:3000/api/authentification/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                emailUtilisateur: email,
+                motsDePasse: password,
+            }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erreur lors de la connexion');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data.idUtilisateur);
+
+                // Stockage dans le sessionStorage
+                sessionStorage.setItem('userID', data.idUtilisateur);
+
+                // Utilisation de la valeur userID récupérée du sessionStorage
+                const userID = sessionStorage.getItem('userID');
+
+                // Utilisation de userID dans d'autres parties du code
+                // Par exemple, pour émettre l'ID utilisateur via le socket
+                socket.emit('user ID', userID);
+
+                // Retourner l'ID utilisateur pour une utilisation éventuelle
+                return data.idUtilisateur;
+            })
+            .catch(error => {
+                console.error('Erreur :', error);
+            });
+    }
+}
+
+
+// _____________________________________________________
+// const userID = 1; // L'ID que vous souhaitez transmettre (pour le moment en dur)
+const userID = handleLogin();
 socket.emit('user ID', userID);
 
 const form = document.getElementById('form');
--- socket.io.js
+++ socket.io.js
@@ -1,33 +1,82 @@
 
+// const socketIO = require('socket.io');
+
+// module.exports = (server) => {
+//     const io = socketIO(server, {
+//         connectionStateRecovery: {} // Ajoutez ici vos options de configuration pour Socket.io
+//     });
+
+//     let idFront = null; // Déclarer idFront en dehors des événements pour y accéder globalement
+
+//     io.on('connection', (socket) => {
+//         console.log('a user connected');
+
+//         socket.on('disconnect', () => {
+//             console.log('user disconnected');
+//         });
+
+//         socket.on('user ID', (userID) => {
+//             idFront = userID; // Affecter la valeur reçue à la variable globale idFront
+//             console.log('userId', userID);
+//             console.log('ID utilisateur reçu:', idFront);
+//         });
+
+//         socket.on('chat message', (msg) => {
+//             console.log('message: ' + msg);
+//             console.log('avant idfront:', idFront);
+
+//             // Utiliser idFront dans votre logique de chat
+//             if (idFront) {
+//                 fetch('http://localhost:3000/api/utilisateur/' + idFront)
+//                     .then(response => response.json())
+//                     .then(user => {
+//                         // Utiliser les données de l'utilisateur reçues ici
+//                         console.log(user.nom);
+//                         io.emit('chat message', `${user.prenom} :  ${msg}`);
+//                     })
+//                     .catch(error => {
+//                         console.error('Erreur lors de la requête :', error);
+//                     });
+//             } else {
+//                 console.log('ID utilisateur non défini');
+//             }
+//         });
+//     });
+// };
+
 const socketIO = require('socket.io');
 
 module.exports = (server) => {
     const io = socketIO(server, {
-        connectionStateRecovery: {} // Ajoutez ici vos options de configuration pour Socket.io
+        connectionStateRecovery: {} // Options de configuration pour Socket.io
     });
 
-    let idFront = null; // Déclarer idFront en dehors des événements pour y accéder globalement
+    const userSockets = {}; // Mappe les ID utilisateur aux sockets correspondantes
 
     io.on('connection', (socket) => {
         console.log('a user connected');
 
         socket.on('disconnect', () => {
             console.log('user disconnected');
+            // Supprimer le mapping lorsque l'utilisateur se déconnecte
+            const userId = userSockets[socket.id];
+            delete userSockets[socket.id];
+            console.log(`User ${userId} disconnected`);
         });
 
         socket.on('user ID', (userID) => {
-            idFront = userID; // Affecter la valeur reçue à la variable globale idFront
-            console.log('userId', userID);
-            console.log('ID utilisateur reçu:', idFront);
+            userSockets[socket.id] = userID; // Associer l'ID utilisateur à la socket
+            console.log('userID:', userID);
+            console.log('Socket ID:', socket.id);
         });
 
         socket.on('chat message', (msg) => {
             console.log('message: ' + msg);
-            console.log('avant idfront:', idFront);
+            const userId = userSockets[socket.id];
 
-            // Utiliser idFront dans votre logique de chat
-            if (idFront) {
-                fetch('http://localhost:3000/api/utilisateur/' + idFront)
+            // Utiliser userId dans votre logique de chat
+            if (userId) {
+                fetch('http://localhost:3000/api/utilisateur/' + userId)
                     .then(response => response.json())
                     .then(user => {
                         // Utiliser les données de l'utilisateur reçues ici
@@ -43,4 +92,3 @@ module.exports = (server) => {
         });
     });
 };
-
